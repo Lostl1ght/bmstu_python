077b7a944d4d66156b8e02a42f73ed953ae5b0fe
bks19u394 <robpirojk@gmail.com>
2020-03-05 20:54:07 +0300

    Added rounding

--- Tk_refinement/test.py
+++ Tk_refinement/test.py
@@ -13,9 +13,9 @@ def iterations(start, interval_end, step, eps):
     def refinement(a, b, eps):
 
         if abs(f(a)) < eps:
-            return a
+            return a, 0
         if abs(f(b)) < eps:
-            return b
+            return b, 0
 
         if f(a) * d2(a) > 0.0:
             c = a
@@ -30,23 +30,31 @@ def iterations(start, interval_end, step, eps):
             x = b
 
         delta = eps + 1.0
-
+        itrs = 0
         while abs(delta) > eps:
+            itrs += 1
             delta = f(x) * (x - c) / (f(x) - f(c))
             x -= delta
 
-        return x
+        return x, itrs
 
     end = start + step
     results = []
+    round_to = abs(int(floor(log10(eps))))
+
     while end < interval_end:
-        itrs = 0
         if f(start) * f(end) <= 0.0:
-            results += [refinement(start, end, eps)]
+            root, itrs = refinement(start, end, eps)
+            if abs(root) < eps:
+                root = 0.0
+            results += [{'root': round(root, round_to), 'itrs': itrs}]
 
         start = end
         end += step
-    results += [refinement(start, interval_end, eps)]
+    root, itrs = refinement(start, end, eps)
+    if abs(root) < eps:
+        root = 0.0
+    results += [{'root': round(root, round_to), 'itrs': itrs}]
 
     return results
 
@@ -56,9 +64,9 @@ def iterations(start, interval_end, step, eps):
 # step = float(input("Step: "))
 # eps = float(input("Eps: "))
 
-start = 0
-interval_end = 10
-step = 3
+start = 1
+interval_end = 5
+step = 4
 eps = 1e-3
 
 results = iterations(start, interval_end, step, eps)
