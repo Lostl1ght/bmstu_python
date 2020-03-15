106ab208204cac86cff4feda5d09eb58013ca8ab
Lostlight <robpirojk@gmail.com>
2020-03-15 13:48:35 +0300

    Added dicts

--- Tk_refinement/refin.py
+++ Tk_refinement/refin.py
@@ -31,8 +31,27 @@ def chord_method(start, end, eps):
 
     return x, iterations
 
-start = -20
-ends = 20
+# def pack(roots, eps):
+#     r = str(abs(int(floor(log10(eps)))))
+#     form = '{:.' + str(r) + 'f}'
+
+#     for i in range(roots):
+
+
+#     i = 1
+#     n = len(roots)
+#     while i < n:
+#         if roots[i][:len(roots[i]) - 2] == roots[i - 1][:len(roots[i - 1]) - 2]:
+#             roots.pop(i - 1)
+#             n -= 1
+#         i += 1
+    
+    
+#     form.format(x)
+
+
+start = 0
+ends = 10
 step = 3
 eps = 1e-3
 
@@ -42,35 +61,24 @@ end = start + step
 while end < ends:
     if abs(f(start)) < eps:
         x, iterations = start, 0
-        roots += []
+        roots += [{'root': x, 'iterations': iterations, 'start': start, 'end': end}]
     elif abs(f(end)) < eps:
         x, iterations = end, 0
-        roots += [x]
+        roots += [{'root': x, 'iterations': iterations, 'start': start, 'end': end}]
     elif f(start) * f(end) < 0:
         x, iterations = chord_method(start, end, eps)
-        roots += [x]
+        roots += [{'root': x, 'iterations': iterations, 'start': start, 'end': end}]
     start, end = end, end + step
 else:
     if abs(f(start)) < eps:
         x, iterations = start, 0
-        roots += [x]
+        roots += [{'root': x, 'iterations': iterations, 'start': start, 'end': end}]
     elif abs(f(ends)) < eps:
         x, iterations = ends, 0
-        roots += [x]
+        roots += [{'root': x, 'iterations': iterations, 'start': start, 'end': end}]
     elif f(start) * f(end) < 0:
         x, iterations = chord_method(start, ends, eps)
-        roots += [x]
-
-# i = 1
-# n = len(roots)
-# while i < n:
-#     if roots[i][:len(roots[i]) - 2] == roots[i - 1][:len(roots[i - 1]) - 2]:
-#         roots.pop(i - 1)
-#         n -= 1
-#     i += 1
-# r = str(abs(int(floor(log10(eps)))))
-# form = '{:.' + str(r) + 'f}'
-# form.format(x)
+        roots += [{'root': x, 'iterations': iterations, 'start': start, 'end': end}]
 
 for i in roots:
     print(i)
