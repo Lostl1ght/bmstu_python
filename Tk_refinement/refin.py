286ef496299e0d813b1799723e01ad03197e17f8
Lostlight <robpirojk@gmail.com>
2020-03-15 14:12:25 +0300

    Formatting

--- Tk_refinement/refin.py
+++ Tk_refinement/refin.py
@@ -10,6 +10,7 @@ def d(x):
     return cos(x)
     # return 2 * x
 
+
 def d2(x):
     return -sin(x)
     # return 2
@@ -19,10 +20,10 @@ def chord_method(start, end, eps):
     iterations = 0
     if d(start) * d2(start) < 0:
         x = start
-        calculate = lambda x: x - f(x) * (end - x) / (f(end) - f(x))
+        def calculate(x): return x - f(x) * (end - x) / (f(end) - f(x))
     else:
         x = end
-        calculate = lambda x: x - f(x) * (start - x) / (f(start) - f(x))
+        def calculate(x): return x - f(x) * (start - x) / (f(start) - f(x))
 
     x_prev, x = x, calculate(x)
     while abs(x - x_prev) >= eps:
@@ -31,29 +32,29 @@ def chord_method(start, end, eps):
 
     return x, iterations
 
-# def pack(roots, eps):
-#     r = str(abs(int(floor(log10(eps)))))
-#     form = '{:.' + str(r) + 'f}'
 
-#     for i in range(roots):
+def pack(roots, eps):
+    r = str(abs(int(floor(log10(eps)))))
+    form = '{:.' + str(r) + 'f}'
 
+    for i in range(len(roots)):
+        if abs(roots[i]['root']) < eps:
+            roots[i]['root'] = abs(roots[i]['root'])
+        roots[i]['root'] = form.format(roots[i]['root'])
 
-#     i = 1
-#     n = len(roots)
-#     while i < n:
-#         if roots[i][:len(roots[i]) - 2] == roots[i - 1][:len(roots[i - 1]) - 2]:
-#             roots.pop(i - 1)
-#             n -= 1
-#         i += 1
-    
-    
-#     form.format(x)
+    i = 1
+    n = len(roots)
+    while i < n:
+        if roots[i]['root'][:len(roots[i]['root']) - 2] == roots[i - 1]['root'][:len(roots[i - 1]['root']) - 2]:
+            roots.pop(i - 1)
+            n -= 1
+        i += 1
 
 
 start = 0
-ends = 10
+ends = 15
 step = 3
-eps = 1e-3
+eps = 1e-4
 
 roots = []
 
@@ -61,24 +62,32 @@ end = start + step
 while end < ends:
     if abs(f(start)) < eps:
         x, iterations = start, 0
-        roots += [{'root': x, 'iterations': iterations, 'start': start, 'end': end}]
+        roots += [{'root': x, 'iterations': iterations,
+                   'start': start, 'end': end}]
     elif abs(f(end)) < eps:
         x, iterations = end, 0
-        roots += [{'root': x, 'iterations': iterations, 'start': start, 'end': end}]
+        roots += [{'root': x, 'iterations': iterations,
+                   'start': start, 'end': end}]
     elif f(start) * f(end) < 0:
         x, iterations = chord_method(start, end, eps)
-        roots += [{'root': x, 'iterations': iterations, 'start': start, 'end': end}]
+        roots += [{'root': x, 'iterations': iterations,
+                   'start': start, 'end': end}]
     start, end = end, end + step
 else:
     if abs(f(start)) < eps:
         x, iterations = start, 0
-        roots += [{'root': x, 'iterations': iterations, 'start': start, 'end': end}]
+        roots += [{'root': x, 'iterations': iterations,
+                   'start': start, 'end': end}]
     elif abs(f(ends)) < eps:
         x, iterations = ends, 0
-        roots += [{'root': x, 'iterations': iterations, 'start': start, 'end': end}]
+        roots += [{'root': x, 'iterations': iterations,
+                   'start': start, 'end': end}]
     elif f(start) * f(end) < 0:
         x, iterations = chord_method(start, ends, eps)
-        roots += [{'root': x, 'iterations': iterations, 'start': start, 'end': end}]
+        roots += [{'root': x, 'iterations': iterations,
+                   'start': start, 'end': end}]
+
+pack(roots, eps)
 
 for i in roots:
     print(i)
