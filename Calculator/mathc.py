19a81115b90c80f231236e00ba74116fb9ddf573
Lostlight <robpirojk@gmail.com>
2020-03-26 16:02:23 +0300

    Subtractor

--- Calculator/mathc.py
+++ Calculator/mathc.py
@@ -1,12 +1,12 @@
-a1 = '1000000.00010100001'
-a2 = '100100.01'
+a1 = '10000101.000101'
+a2 = '10100.001'
 
 
 print('Input nums:')
 print(a1, '\n', a2, sep='')
 
 
-def converter(a1, a2):    
+def converter(a1, a2):
     c1 = ''
     for i in a1:
         if i == '.':
@@ -17,14 +17,14 @@ def converter(a1, a2):
         if i == '.':
             continue
         c2 += i
-    
+
     dot_len_a1 = 0
     if '.' in a1:
         dot_len_a1 = len(a1[a1.index('.'):]) - 1
     dot_len_a2 = 0
     if '.' in a2:
         dot_len_a2 = len(a2[a2.index('.'):]) - 1
-    
+
     if dot_len_a1 > dot_len_a2:
         c2 += '0' * (dot_len_a1 - dot_len_a2)
         dot_len = dot_len_a1
@@ -41,9 +41,9 @@ def converter(a1, a2):
     len_c2 = len(c2)
     if '-' in c2:
         len_c2 -= 1
-    
-    while length <= max(len_c1, len_c2):
-        length += 8
+
+    while length < max(len_c1, len_c2):
+        length += 1
 
     bb1 = c1[::-1] + '0' * (length - len_c1)
 
@@ -51,7 +51,23 @@ def converter(a1, a2):
 
     return bb1, bb2, length, dot_len
 
-def adder(b1, b2, length, dot_len):
+
+def normalize(c, dot_len, minus):
+    k = 0
+    if dot_len > 0:
+        c = c[:dot_len] + '.' + c[dot_len:]
+        while c[k] == '0':
+            k += 1
+    c = c[::-1]
+    i = 0
+    while c[i] == '0':
+        i += 1
+    if minus:
+        return '-' + c[i:len(c) - k]
+    return c[i:len(c) - k]
+
+
+def adder(b1, b2, length):
     c = ''
     flag = False
     for i in range(length):
@@ -61,7 +77,7 @@ def adder(b1, b2, length, dot_len):
             else:
                 c += '0'
             flag = False
-            
+
         if b1[i] == '1' and b2[i] == '0' or b1[i] == '0' and b2[i] == '1':
             if flag:
                 c += '0'
@@ -78,24 +94,53 @@ def adder(b1, b2, length, dot_len):
     if flag:
         c += '1'
 
-    return c
+    return c, False
 
-def normalize(c, dot_len):
-    k = 0
-    if dot_len > 0:
-        c = c[:dot_len] + '.' + c[dot_len:]
-        while c[k] == '0':
-            k += 1
-    c = c[::-1]
-    i = 0
-    while c[i] == '0':
-        i += 1
-    
-    return c[i:len(c) - k]
+
+def subtractor(b1, b2, length):
+    minus = False
+    if b1[::-1] < b2[::-1]:
+        b1, b2 = b2, b1
+        minus = True
+    c = ''
+    flag = False
+    for i in range(length):        
+        if b1[i] == '0' and b2[i] == '0':            
+            if flag:
+                c += '1'
+                flag = True
+            else:
+                c += '0'
+                flag = False
+
+        if b1[i] == '1' and b2[i] == '0':
+            if flag:
+                c += '0'
+                
+            else:
+                c += '1'
+            flag = False
+
+        if b1[i] == '0' and b2[i] == '1':
+            if flag:
+                c += '0'
+            else:
+                c += '1'
+            flag = True
+        
+        if b1[i] == '1' and b2[i] == '1':
+            c += '0'
+
+    return c, minus
 
 
 b1, b2, length, dot_len = converter(a1, a2)
-c = adder(b1, b2, length, dot_len)
-result = normalize(c, dot_len)
+c, minus = subtractor(b1, b2, length)
+res = normalize(c, dot_len, minus)
 
-print('\nSum:', result)
+print('\nSubt:', res)
+
+c, minus = adder(b1, b2, length)
+res = normalize(c, dot_len, minus)
+
+print('\nSum:', res)
