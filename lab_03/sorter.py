0483c800a06060198f4d9bdfe136c4224ae38309
Lostlight <robpirojk@gmail.com>
2020-04-01 23:10:51 +0300

    Finished interface

--- lab_03/sorter.py
+++ lab_03/sorter.py
@@ -1,4 +1,4 @@
-from tkinter import *
+from tkinter import W, Frame, Tk, Label, Entry, Button
 from tkinter.ttk import Treeview, Style
 
 import matplotlib.pyplot as plt
@@ -61,7 +61,7 @@ def create_blank() -> None:
     canvas.draw()
     canvas.get_tk_widget().pack()
 
-    Style().configure('Treeview', rowheight=40)
+    Style().configure('Treeview', rowheight=60)
 
     tree = Treeview(table_frame, columns=('names', 'n1', 'n2', 'n3'), height=3)
 
@@ -89,7 +89,7 @@ def create_blank() -> None:
 def create_graph(graph_frame: 'Frame') -> None:
     graph_frame.pack_forget()
     graph_frame = Frame(window)
-    graph_frame.grid(row=3)
+    graph_frame.grid(row=2, column=1)
 
     x = [2000, 2500, 3000]
     y = [1, 10, 3]
@@ -107,12 +107,12 @@ def create_graph(graph_frame: 'Frame') -> None:
     canvas.get_tk_widget().pack()
 
 
-def sort() -> None:
+def sort_small() -> None:
     try:
-        if n_array_entry.get() == '':
+        if array_entry.get() == '':
             n_array_label['text'] = 'ERROR'
             return
-        n_array = list(int(i) for i in n_array_entry.get().split())
+        n_array = list(int(i) for i in array_entry.get().split())
         ns_array = ', '.join(str(s) for s in n_array)
         n_array_label['text'] = ns_array
         insertion_bin(n_array)
@@ -176,46 +176,73 @@ def count_down(n: int, leng: int) -> float:
 
     return summ / leng
 
+
 window = Tk()
 window.title('Sorting methods research')
 window.resizable(0, 0)
 
-test_frame = Frame(window)
-test_frame.grid(row=0)
-ns_frame = Frame(window)
-ns_frame.grid(row=1)
+array_frame = Frame(window)
+array_frame.grid(row=0, columnspan=2)
+table_input_frame = Frame(window)
+table_input_frame.grid(row=1, column=0)
 table_frame = Frame(window)
-table_frame.grid(row=2)
+table_frame.grid(row=1, column=1)
+graph_input_frame = Frame(window)
+graph_input_frame.grid(row=2, column=0)
 graph_frame = Frame(window)
-graph_frame.grid(row=3)
+graph_frame.grid(row=2, column=1)
 
 tree = create_blank()
 
-a_label = Label(test_frame, text='Array', width=30)
-a_label.grid(row=0, column=0)
-n_array_label = Label(test_frame, text='', width=30)
+array_label = Label(array_frame, text='Array', width=30)
+array_label.grid(row=0, column=0)
+array_entry = Entry(array_frame, width=36)
+array_entry.grid(row=1, column=0)
+array_btn = Button(array_frame, text='Input', width=15, command=sort_small)
+array_btn.grid(row=1, column=1, rowspan=3)
+n_array_label = Label(array_frame, text='', width=30, background='#ffffff')
 n_array_label.grid(row=2, column=0)
-s_array_label = Label(test_frame, text='', width=30)
+s_array_label = Label(array_frame, text='', width=30, background='#ffffff')
 s_array_label.grid(row=3, column=0)
+blank = Label(array_frame, text='', width=30)
+blank.grid(row=4, column=0)
 
-n_array_entry = Entry(test_frame, width=30)
-n_array_entry.grid(row=1, column=0)
+tit_label = Label(table_input_frame, text='Num of measurements', width=18)
+tit_label.grid(row=0)
+tit_entry = Entry(table_input_frame, width=20)
+tit_entry.grid(row=1)
+n1_label = Label(table_input_frame, text='N1', width=15)
+n1_label.grid(row=2)
+n1_entry = Entry(table_input_frame, width=20)
+n1_entry.grid(row=3)
+n2_label = Label(table_input_frame, text='N2', width=15)
+n2_label.grid(row=4)
+n2_entry = Entry(table_input_frame, width=20)
+n2_entry.grid(row=5)
+n3_label = Label(table_input_frame, text='N3', width=15)
+n3_label.grid(row=6)
+n3_entry = Entry(table_input_frame, width=20)
+n3_entry.grid(row=7)
+table_btn = Button(table_input_frame, text='Input', width=15)
+table_btn.grid(row=8)
+table_status_label = Label(table_input_frame, text='1', width=15)
+table_status_label.grid(row=9)
 
-test_btn = btn = Button(test_frame, text='Input', width=15, command=sort)
-test_btn.grid(row=1, column=1)
-
-er_label = Label(ns_frame, text='', width=30)
-start_label = Label(ns_frame, text='N1', width=10)
-start_label.grid(row=0, column=0)
-step_label = Label(ns_frame, text='Step', width=10)
-step_label.grid(row=0, column=1)
-
-start_entry = Entry(ns_frame, width=20)
-start_entry.grid(row=1, column=0)
-step_entry = Entry(ns_frame, width=20)
-step_entry.grid(row=1, column=1)
-
-btn = Button(ns_frame, text='Input', width=15, command=do)
-btn.grid(row=1, column=3)
+git_label = Label(graph_input_frame, text='Num of measurements', width=18)
+git_label.grid(row=0)
+git_entry = Entry(graph_input_frame, width=20)
+git_entry.grid(row=1)
+n_label = Label(graph_input_frame, text='N', width=15)
+n_label.grid(row=2)
+n_entry = Entry(graph_input_frame, width=20)
+n_entry.grid(row=3)
+step_label = Label(graph_input_frame, text='Step', width=15)
+step_label.grid(row=4)
+step_entry = Entry(graph_input_frame, width=20)
+step_entry.grid(row=5)
+graph_btn = Button(graph_input_frame, text='Input', width=15, command=do)
+graph_btn.grid(row=6)
+graph_status_label = Label(graph_input_frame, text='1', width=15)
+graph_status_label.grid(row=7)
 
 window.mainloop()
