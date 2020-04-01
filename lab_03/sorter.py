d9a5385b6d9118214527f797170d8498644efebc
Lostlight <robpirojk@gmail.com>
2020-04-02 00:35:56 +0300

    Table w/out status bar

--- lab_03/sorter.py
+++ lab_03/sorter.py
@@ -7,11 +7,6 @@ import time
 from random import randint
 
 
-def do() -> None:
-    create_table(tree)
-    create_graph(graph_frame)
-
-
 def insertion_bin(array: list) -> None:
     def binsearch(array: list, value: int, start: int, end: int) -> int:
         if start >= end:
@@ -35,7 +30,7 @@ def insertion_bin(array: list) -> None:
             array[j], array[j - 1] = array[j - 1], array[j]
 
 
-def create_table(tree: 'Treeview') -> None:
+def create_table(tree: 'Treeview', rand: list, up: list, down: list) -> None:
     tree.delete(*tree.get_children())
 
     text = ['Упорядоченный\nмассив\n', 'Случайный\nмассив\n',
@@ -45,13 +40,14 @@ def create_table(tree: 'Treeview') -> None:
     tree.heading('n2', text='2500', anchor=W)
     tree.heading('n3', text='3000', anchor=W)
 
-    for n in range(3):
-        tree.insert('', n, values=(text[n], '1', '2', '3'))
+    tree.insert('', 0, values=(text[0], up[0], up[1], up[2]))
+    tree.insert('', 1, values=(text[1], rand[0], rand[1], rand[2]))
+    tree.insert('', 2, values=(text[2], down[0], down[1], down[2]))
 
     tree.pack()
 
 
-def create_blank() -> None:
+def create_blank() -> 'Treeview':
     fig = plt.Figure(figsize=(6, 3), dpi=100)
     ax = fig.add_subplot(111)
     ax.plot()
@@ -128,53 +124,62 @@ def gettimeofday() -> int:
 
 def count_random(n: int, leng: int) -> float:
     a = []
-    for i in range(n):
+    for i in range(leng):
         a.append(randint(-1000, 1000))
 
     summ = 0
-    for i in range(leng):
-        print('Try', i + 1)
+    for i in range(n):
         start = gettimeofday()
         insertion_bin(a[:])
         end = gettimeofday()
-        print(end - start)
         summ += end - start
-
-    return summ / leng
+    return summ / n
 
 
 def count_up(n: int, leng: int) -> float:
     a = []
-    for i in range(n):
+    for i in range(leng):
         a.append(i + 1)
 
     summ = 0
-    for i in range(leng):
-        print('Try', i + 1)
+    for i in range(n):
         start = gettimeofday()
         insertion_bin(a[:])
         end = gettimeofday()
-        print(end - start)
         summ += end - start
-
-    return summ / leng
+    return summ / n
 
 
 def count_down(n: int, leng: int) -> float:
     a = []
-    for i in range(n, 0, -1):
+    for i in range(leng, 0, -1):
         a.append(i + 1)
 
     summ = 0
-    for i in range(leng):
-        print('Try', i + 1)
+    for i in range(n):
         start = gettimeofday()
         insertion_bin(a[:])
         end = gettimeofday()
-        print(end - start)
         summ += end - start
+    return summ / n
 
-    return summ / leng
+
+def calculate_for_table(meas: int, n: list) -> None:
+    table_status_label['text'] = 'STARTING'
+    rand = []
+    for i in range(3):
+        rand.append(count_random(meas, n[i]))
+
+    up = []
+    for i in range(3):
+        up.append(count_up(meas, n[i]))
+
+    down = []
+    for i in range(3):
+        down.append(count_down(meas, n[i]))
+    s = 1
+    create_table(tree, rand, up, down)
+    table_status_label['text'] = 'DONE'
 
 
 window = Tk()
@@ -223,10 +228,11 @@ n3_label = Label(table_input_frame, text='N3', width=15)
 n3_label.grid(row=6)
 n3_entry = Entry(table_input_frame, width=20)
 n3_entry.grid(row=7)
-table_btn = Button(table_input_frame, text='Input', width=15)
+table_btn = Button(table_input_frame, text='Input', width=15, command=lambda: calculate_for_table(
+    int(tit_entry.get()), [int(n1_entry.get()), int(n2_entry.get()), int(n3_entry.get())]))
 table_btn.grid(row=8)
-table_status_label = Label(table_input_frame, text='1', width=15)
-table_status_label.grid(row=9)
+# table_status_label = Label(table_input_frame, text='', width=15)
+# table_status_label.grid(row=9)
 
 git_label = Label(graph_input_frame, text='Num of measurements', width=18)
 git_label.grid(row=0)
@@ -240,9 +246,10 @@ step_label = Label(graph_input_frame, text='Step', width=15)
 step_label.grid(row=4)
 step_entry = Entry(graph_input_frame, width=20)
 step_entry.grid(row=5)
-graph_btn = Button(graph_input_frame, text='Input', width=15, command=do)
+graph_btn = Button(graph_input_frame, text='Input', width=15,
+                   command=lambda: create_graph(graph_frame))
 graph_btn.grid(row=6)
-graph_status_label = Label(graph_input_frame, text='1', width=15)
-graph_status_label.grid(row=7)
+# graph_status_label = Label(graph_input_frame, text='', width=15)
+# graph_status_label.grid(row=7)
 
 window.mainloop()
