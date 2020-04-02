b048ab43496098d068cb1dabaebfe820ce76c839
Lostlight <robpirojk@gmail.com>
2020-04-02 03:34:55 +0300

    Finished

--- lab_03/sorter.py
+++ lab_03/sorter.py
@@ -1,5 +1,6 @@
 # Сортировка с бинарным поиском.
 
+
 from tkinter import W, Frame, Tk, Label, Entry, Button, Toplevel, HORIZONTAL
 from tkinter.ttk import Treeview, Style, Progressbar
 
@@ -28,7 +29,6 @@ def insertion_bin(array: list) -> None:
             return mid
 
     for i in range(1, len(array)):
-        window.update_idletasks()
         pos = binsearch(array, array[i], 0, i - 1)
         for j in range(i, pos, -1):
             array[j], array[j - 1] = array[j - 1], array[j]
@@ -38,8 +38,7 @@ def insertion_bin(array: list) -> None:
 def create_table(tree: 'Treeview', rand: list, up: list, down: list) -> None:
     tree.delete(*tree.get_children())
 
-    text = ['Упорядоченный\nпо возрастанию\nмассив\n', 'Случайный\nмассив\n',
-            'Упорядоченный\nпо убыванию\nмассив\n']
+    text = ['Ascending array', 'Random array', 'Descending array']
 
     tree.heading('n1', text=n1_entry.get(), anchor=W)
     tree.heading('n2', text=n2_entry.get(), anchor=W)
@@ -57,7 +56,7 @@ def create_table(tree: 'Treeview', rand: list, up: list, down: list) -> None:
 
 # Создание пустых графика и таблицы.
 def create_blank() -> 'Treeview':
-    fig = plt.Figure(figsize=(6, 3), dpi=100)
+    fig = plt.Figure(figsize=(6, 4), dpi=100)
     ax = fig.add_subplot(111)
     ax.plot()
     ax.grid()
@@ -81,8 +80,7 @@ def create_blank() -> 'Treeview':
     tree.heading('n2', text='N2', anchor=W)
     tree.heading('n3', text='N3', anchor=W)
 
-    text = ['Упорядоченный\nпо возрастанию\nмассив\n', 'Случайный\nмассив\n',
-            'Упорядоченный\nпо убыванию\nмассив\n']
+    text = ['Ascending array', 'Random array', 'Descending array']
 
     tree.insert('', 0, values=(text[0], '', '', ''))
     tree.insert('', 1, values=(text[1], '', '', ''))
@@ -95,24 +93,46 @@ def create_blank() -> 'Treeview':
 
 # Рисование графика.
 def create_graph(graph_frame: 'Frame') -> None:
-    graph_frame.pack_forget()
-    graph_frame = Frame(window)
-    graph_frame.grid(row=2, column=1)
+    try:
+        gerr_label['text'] = ''
+        graph_frame.pack_forget()
+        graph_frame = Frame(window)
+        graph_frame.grid(row=2, column=1)
 
-    x = [2000, 2500, 3000]
-    y = [1, 10, 3]
+        x = []
+        n = int(n_entry.get())
+        step = int(step_entry.get())
+        status = 0
+        for i in range(10):
+            x.append(n)
+            graph_progress['value'] = status
+            window.update()
+            status += 1
+            n += step
 
-    fig = plt.Figure(figsize=(6, 3), dpi=100)
+        status = 10
+        y = []
+        for i in x:
+            y.append(count_random(2, i))
+            graph_progress['value'] = status
+            window.update()
+            status += 9
 
-    ax = fig.add_subplot(111)
+        fig = plt.Figure(figsize=(6, 4), dpi=100)
 
-    ax.plot(x, y)
-    ax.set_yticks(y)
-    ax.set_xticks(x)
+        ax = fig.add_subplot(111)
 
-    canvas = FigureCanvasTkAgg(fig, master=graph_frame)
-    canvas.draw()
-    canvas.get_tk_widget().pack()
+        ax.plot(x, y)
+        ax.set_yticks(y)
+        ax.set_xticks(x)
+
+        canvas = FigureCanvasTkAgg(fig, master=graph_frame)
+        canvas.draw()
+        canvas.get_tk_widget().pack()
+        graph_progress['value'] = 100
+        window.update()
+    except:
+        gerr_label['text'] = 'ERROR'
 
 
 # Сортировка маленького массива.
@@ -144,7 +164,6 @@ def count_random(n: int, leng: int) -> int:
 
     summ = 0
     for i in range(n):
-        window.update_idletasks()
         start = gettimeofday()
         insertion_bin(a[:])
         end = gettimeofday()
@@ -160,7 +179,6 @@ def count_up(n: int, leng: int) -> int:
 
     summ = 0
     for i in range(n):
-        window.update_idletasks()
         start = gettimeofday()
         insertion_bin(a[:])
         end = gettimeofday()
@@ -176,7 +194,6 @@ def count_down(n: int, leng: int) -> int:
 
     summ = 0
     for i in range(n):
-        window.update_idletasks()
         start = gettimeofday()
         insertion_bin(a[:])
         end = gettimeofday()
@@ -186,34 +203,39 @@ def count_down(n: int, leng: int) -> int:
 
 # Вычисление времени и заполнение таблицы.
 def calculate_for_table(meas: int, n: list) -> None:
-    progress['value'] = 5
-    window.update_idletasks()
-    status = 0
+    try:
+        n = [int(i) for i in n]
+        terr_label['text'] = ''
+        table_progress['value'] = 5
+        window.update()
+        status = 0
 
-    up = []
-    for i in range(3):
-        status += 10
-        progress['value'] = status
-        window.update_idletasks()
-        up.append(count_up(meas, n[i]))
+        up = []
+        for i in range(3):
+            status += 10
+            table_progress['value'] = status
+            window.update()
+            up.append(count_up(meas, n[i]))
 
-    rand = []
-    for i in range(3):
-        status += 10
-        progress['value'] = status
-        window.update_idletasks()
-        rand.append(count_random(meas, n[i]))
+        rand = []
+        for i in range(3):
+            status += 10
+            table_progress['value'] = status
+            window.update()
+            rand.append(count_random(meas, n[i]))
 
-    down = []
-    for i in range(3):
-        status += 10
-        progress['value'] = status
-        window.update_idletasks()
-        down.append(count_down(meas, n[i]))
+        down = []
+        for i in range(3):
+            status += 10
+            table_progress['value'] = status
+            window.update()
+            down.append(count_down(meas, n[i]))
 
-    create_table(tree, rand, up, down)
-    progress['value'] = 100
-    window.update_idletasks()
+        create_table(tree, rand, up, down)
+        table_progress['value'] = 100
+        window.update()
+    except:
+        terr_label['text'] = 'ERROR'
 
 
 window = Tk()
@@ -250,6 +272,8 @@ blank.grid(row=4, column=0)
 # tit_label.grid(row=0)
 # tit_entry = Entry(table_input_frame, width=20)
 # tit_entry.grid(row=1)
+terr_label = Label(table_input_frame, text='', width=18)
+terr_label.grid(row=0)
 n1_label = Label(table_input_frame, text='N1', width=15)
 n1_label.grid(row=2)
 n1_entry = Entry(table_input_frame, width=20)
@@ -263,7 +287,7 @@ n3_label.grid(row=6)
 n3_entry = Entry(table_input_frame, width=20)
 n3_entry.grid(row=7)
 table_btn = Button(table_input_frame, text='Input', width=15, command=lambda: calculate_for_table(
-    2, [int(n1_entry.get()), int(n2_entry.get()), int(n3_entry.get())]))
+    2, [n1_entry.get(), n2_entry.get(), n3_entry.get()]))
 table_btn.grid(row=8)
 table_progress = Progressbar(table_input_frame, orient=HORIZONTAL,
                              length=100, mode='determinate')
@@ -273,6 +297,8 @@ table_progress.grid(row=9)
 # git_label.grid(row=0)
 # git_entry = Entry(graph_input_frame, width=20)
 # git_entry.grid(row=1)
+gerr_label = Label(graph_input_frame, text='', width=18)
+gerr_label.grid(row=0)
 n_label = Label(graph_input_frame, text='N', width=15)
 n_label.grid(row=2)
 n_entry = Entry(graph_input_frame, width=20)
