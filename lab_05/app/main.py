0f61cd77fb1b76754189249a1d00a25566a83f77
Lostlight <robpirojk@gmail.com>
2020-05-14 03:52:20 +0300

    Car crash

--- lab_05/app/main.py
+++ lab_05/app/main.py
@@ -5,34 +5,35 @@ import pygame
 class Car:
     EMPTY = pygame.Color(0, 0, 0, 0)
     l = 1
+    tick = 0
 
-    def __init__(self, x, y, carcass, wheel1, wheel2):
-        self.car = pygame.image.load(carcass).convert_alpha()
+    def __init__(self, x, y, car_images, crash_images):
+        self.car = pygame.image.load(car_images + '\car.png').convert_alpha()
 
         self.base_surf = pygame.Surface((self.car.get_width(),
                                          self.car.get_height()), flags=pygame.SRCALPHA)
         self.base_surf_rect = self.base_surf.get_rect(topleft=(x, y))
 
-        self.wheel1 = [pygame.image.load(wheel1).convert_alpha(),
-                       pygame.image.load(wheel2).convert_alpha()]
+        self.wheel1 = [pygame.image.load(car_images + '\wheel1.png').convert_alpha(),
+                       pygame.image.load(car_images + '\wheel2.png').convert_alpha()]
         self.wheel1_rect = [self.wheel1[0].get_rect(center=(40, 75)),
                             self.wheel1[1].get_rect(center=(40, 75))]
 
-        self.wheel2 = [pygame.image.load(wheel1).convert_alpha(),
-                       pygame.image.load(wheel2).convert_alpha()]
+        self.wheel2 = [pygame.image.load(car_images + '\wheel1.png').convert_alpha(),
+                       pygame.image.load(car_images + '\wheel2.png').convert_alpha()]
         self.wheel2_rect = [self.wheel2[0].get_rect(center=(170, 75)),
                             self.wheel2[1].get_rect(center=(170, 75))]
+
+        self.crash = [pygame.image.load(crash_images + '\crash1.png').convert_alpha(),
+                      pygame.image.load(crash_images + '\crash2.png').convert_alpha()]
+        self.crash_rect = [self.crash[0].get_rect(), self.crash[1].get_rect()]
 
         self.base_surf.fill(self.EMPTY)
         self.base_surf.blit(self.car, (0, 0))
         self.base_surf.blit(self.wheel1[0], self.wheel1_rect[0])
 
-    def move(self, dist):
-        if self.base_surf_rect.x < dist - self.car.get_width():
-            self.base_surf_rect.x += 5
-        else:
-            self.base_surf_rect.x = 0
-
+    def move(self):        
+        car.base_surf_rect.x += 5
         self.base_surf.fill(self.EMPTY)
 
         self.base_surf.blit(self.car, (0, 0))
@@ -40,19 +41,30 @@ class Car:
         self.base_surf.blit(self.wheel1[self.l], self.wheel1_rect[self.l])
         self.l = (self.l + 1) % 2
 
+    def burn(self):        
+        self.tick += 1
+        if self.tick != 6:
+            return
+        self.tick = 0
+        
+        self.base_surf.fill(self.EMPTY)
+
+        self.base_surf.blit(self.crash[self.l], self.crash_rect[self.l])
+        self.l = (self.l + 1) % 2
+
 
 pygame.init()
-W = 600
+W = 500
 H = 200
 WHITE = (255, 255, 255)
+DIST = 300
 
 sc = pygame.display.set_mode((W, H))
 
 bg = pygame.Surface((W, H))
 bg_rect = bg.get_rect()
 
-car = Car(0, 90, 'moving_car\car.png',
-          'moving_car\wheel1.png', 'moving_car\wheel2.png')
+car = Car(0, 90, 'moving_car', 'car_crash')
 
 bg.fill(WHITE)
 bg.blit(car.base_surf, car.base_surf_rect)
@@ -61,7 +73,7 @@ sc.blit(bg, bg_rect)
 pygame.display.update()
 
 while True:
-    pygame.time.delay(25)
+    pygame.time.delay(100)
 
     for i in pygame.event.get():
         if i.type == pygame.QUIT:
@@ -70,7 +82,10 @@ while True:
     sc.fill(WHITE)
     bg.fill(WHITE)
 
-    car.move(W)
+    if car.base_surf_rect.x < DIST - car.car.get_width():
+        car.move()
+    else:
+        car.burn()
 
     bg.blit(car.base_surf, car.base_surf_rect)
     sc.blit(bg, bg_rect)
