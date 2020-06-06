f4dd4cb406cd2de4138034f94eb08445789a19d6
Lostlight <robpirojk@gmail.com>
2020-06-07 01:34:22 +0300

    Upgraded vigenere

--- term_01/labs/17. Виженер.py
+++ term_01/labs/17. Виженер.py
@@ -1,4 +1,7 @@
 operation = int(input("Введите номер операции (1. Зашифровать, 2. Расшифровать): "))
+start = 33
+end = 122
+length = end - start + 1
 if operation == 1:
     key_word = input("Введите ключевое слово: ")
     word = input("Введите слово, которое хотите зашифровать: ")
@@ -6,16 +9,16 @@ if operation == 1:
     n_key_words = len(word) // len(key_word)
     helping_word = key_word * n_key_words + key_word[:len(word) % len(key_word)]
 
-    # Алфавит от 97 до 122
+    # Алфавит от start до end
     chiffre = ""
     for i in range(len(word)):
-        line = ord(helping_word[i]) - 97
-        column = ord(word[i]) - 97
+        line = ord(helping_word[i]) - start
+        column = ord(word[i]) - start
 
-        if 97 + line + column <= 122:
-            symbol = chr(97 + line + column)
+        if start + line + column <= end:
+            symbol = chr(start + line + column)
         else:
-            symbol = chr(97 + line + column - 26)
+            symbol = chr(start + line + column - length)
         chiffre = chiffre + symbol
 
     print(chiffre)
@@ -28,12 +31,12 @@ else:
 
     message = ""
     for i in range(len(word)):
-        line = ord(helping_word[i]) - 97
-        letter = ord(word[i]) - 97
+        line = ord(helping_word[i]) - start
+        letter = ord(word[i]) - start
         column = letter - line
         if column < 0:
-            column = column + 26
-        symbol = chr(97 + column)
+            column = column + length
+        symbol = chr(start + column)
         message = message + symbol
 
     print(message)
